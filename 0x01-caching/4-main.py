de3c29f51f52de709ba09511bab8663cc68a827b
Im-adom <graceamaningkwarteng.com>
2024-03-05 22:49:33 +0000

    The caching tasks

---
+++ 0x01-caching/4-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 4-main """
+MRUCache = __import__('4-mru_cache').MRUCache
+
+my_cache = MRUCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+
